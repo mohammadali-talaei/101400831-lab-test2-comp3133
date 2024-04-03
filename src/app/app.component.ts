27e46e4cd81691579ac824c2262a8d577dabbc75
developer-ipark <mohammadali.talaei@georgebrown.ca>
2024-04-03 16:23:36 -0400

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.scss'
+})
+export class AppComponent {
+  title = '101400831-lab-test2-comp3133';
+}
